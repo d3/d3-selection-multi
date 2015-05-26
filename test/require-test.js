3be7d11232daf5f727f7b099ee66ea8721d2bad6
Mike Bostock <mbostock@gmail.com>
2015-05-26 14:49:14 -0700

    Adopt new module pattern.

--- test/require-test.js
+++ test/require-test.js
@@ -3,19 +3,23 @@ var tape = require("tape"),
     requirejs = require("requirejs");
 
 tape("can load as AMD module", function(test) {
+  delete global.d3;
   requirejs([path.join(__dirname, "../node_modules/d3-selection/d3-selection")], function(d3) {
     requirejs([path.join(__dirname, "../d3-selection-multi")], function() {
-      test.equal(typeof d3.selection.prototype.attrs, "function", "d3.selection.prototype.attrs is a function");
+      test.equal(typeof d3.selection.prototype.attrs, "function");
       test.end();
+      delete global.d3;
     });
   });
 });
 
 tape("can load as minified AMD module", function(test) {
+  delete global.d3;
   requirejs([path.join(__dirname, "../node_modules/d3-selection/d3-selection.min")], function(d3) {
     requirejs([path.join(__dirname, "../d3-selection-multi.min")], function() {
-      test.equal(typeof d3.selection.prototype.attrs, "function", "d3.selection.prototype.attrs is a function");
+      test.equal(typeof d3.selection.prototype.attrs, "function");
       test.end();
+      delete global.d3;
     });
   });
 });
