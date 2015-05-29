326c44844146bbb784be47fac52d944e84bf2360
Mike Bostock <mbostock@gmail.com>
2015-05-28 21:45:07 -0700

    Remove global.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+import {selection} from "d3-selection";
 import selection_attrs from "./src/selection_attrs";
 import selection_styles from "./src/selection_styles";
 import selection_properties from "./src/selection_properties";
 
-var selection = d3.selection;
 selection.prototype.attrs = selection_attrs;
 selection.prototype.styles = selection_styles;
 selection.prototype.properties = selection_properties;
