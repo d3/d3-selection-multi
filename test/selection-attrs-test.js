d77b39ac8cdedffa09077a5e305f7bcc7a2c5264
Mike Bostock <mbostock@gmail.com>
2015-11-03 23:50:14 -0800

    Upgrade d3-bundler.

--- test/selection-attrs-test.js
+++ test/selection-attrs-test.js
@@ -1,13 +1,15 @@
 var tape = require("tape"),
     jsdom = require("jsdom"),
-    d3 = require("../");
+    selection = require("d3-selection");
+
+require("../");
 
 tape("selection.attrs sets attributes on selected elements", function(test) {
   var document = jsdom.jsdom();
-  d3.select(document.body).attrs({foo: 42, bar: "BAR"});
+  selection.select(document.body).attrs({foo: 42, bar: "BAR"});
   test.equal(document.body.getAttribute("foo"), "42");
   test.equal(document.body.getAttribute("bar"), "BAR");
-  d3.select(document.body).attrs({foo: null});
+  selection.select(document.body).attrs({foo: null});
   test.equal(document.body.hasAttribute("foo"), false);
   test.end();
 });
@@ -16,7 +18,7 @@ tape("selection.attrs can take a function that returns an object", function(test
   var document = jsdom.jsdom("<h1>hello</h1><h2>world</h2>"),
       h1 = document.querySelector("h1"),
       h2 = document.querySelector("h2");
-  d3.selectAll([h1, h2]).attrs(function(d, i) { return !i ? {foo: 42} : {bar: "BAR"}; });
+  selection.selectAll([h1, h2]).attrs(function(d, i) { return !i ? {foo: 42} : {bar: "BAR"}; });
   test.equal(h1.getAttribute("foo"), "42");
   test.equal(h1.hasAttribute("bar"), false);
   test.equal(h2.hasAttribute("foo"), false);
@@ -28,7 +30,7 @@ tape("selection.attrs can take an object with function values", function(test) {
   var document = jsdom.jsdom("<h1>hello</h1><h2>world</h2>"),
       h1 = document.querySelector("h1"),
       h2 = document.querySelector("h2");
-  d3.selectAll([h1, h2]).attrs({
+  selection.selectAll([h1, h2]).attrs({
     foo: function(d, i) { return !i ? 42 : null; },
     bar: function(d, i) { return !i ? null : "BAR"; },
   });
